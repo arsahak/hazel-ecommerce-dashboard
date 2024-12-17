8dae00b4b9ceb7a485465e11856196e83a87b2f9
arsahak <arsahak99@gmail.com>
2024-12-17 22:06:30 +0600

    add new page

--- app/(page)/page.tsx
+++ app/(page)/page.tsx
@@ -2,16 +2,14 @@ import { auth } from "@/auth";
 import DashboardHeader from "@/components/dashboard/DashboardHeader";
 import SalesReport from "@/components/dashboard/SalesReport";
 import ShopDetialsSection from "@/components/dashboard/ShopDetialsSection";
-import { redirect } from "next/navigation";
 
 const page = async () => {
   const session = await auth();
 
-  if (!session) {
-    redirect("/sign-in");
-  }
+  // if (!session) {
+  //   redirect("/sign-in");
+  // }
 
-  console.log("check value of login", session);
   return (
     <div className="bg-[#f8f8f8] mx-10 ">
       <DashboardHeader />
